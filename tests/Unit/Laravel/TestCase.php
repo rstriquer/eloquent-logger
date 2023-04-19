b0134b247aea2861d8465eecef336e1597b02cb1
rstriquer <rstriquer@gmail.com>
2023-04-19 13:01:41 -0300

    test: Add unit test base to project

---
+++ tests/Unit/Laravel/TestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace RStriquer\EloquentLogger\Tests\Unit\Laravel;
+
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Creates a new instance of Laravel Application.
+     *
+     * @return \PHPUnit\Framework\MockObject\MockObject
+     */
+    public function createApplication()
+    {
+        $app = $this->createPartialMock(Application::class, ['runningInConsole', 'runningUnitTests']);
+
+        Container::setInstance($app);
+
+        $app->singleton(
+            ExceptionHandler::class,
+            function () use ($app) {
+                return new \Illuminate\Foundation\Exceptions\Handler($app);
+            }
+        );
+
+        return $app;
+    }
+}
