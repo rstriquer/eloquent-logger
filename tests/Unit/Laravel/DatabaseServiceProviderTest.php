9500b20ef48d15dca59453b8a29b2884d54c0f01
rstriquer <rstriquer@gmail.com>
2023-04-19 13:02:25 -0300

    test: Add first effective unit test to project

---
+++ tests/Unit/Laravel/DatabaseServiceProviderTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+// namespace Tests\Unit\Laravel;
+
+namespace RStriquer\EloquentLogger\Tests\Unit\Laravel;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use RStriquer\EloquentLogger\Adapters\Laravel\DatabaseServiceProvider;
+use RStriquer\EloquentLogger\Tests\Unit\Laravel\Models\User;
+
+class AuthServiceProviderTest extends TestCase
+{
+    protected $app_mock;
+
+    public static function setUpBeforeClass(): void
+    {
+        if (getenv('DB_LOGGER_ACTIVE') || getenv('DB_LOGGER_FILE')) {
+            throw new InvalidArgumentException(
+                'Neither DB_LOGGER_ACTIVE nor DB_LOGGER_FILE should be set'
+                .' on test environment'
+            );
+        }
+        putenv('DB_LOGGER_ACTIVE="/logs/query.log"');
+        parent::setUpBeforeClass();
+    }
+
+    public function setUp(): void
+    {
+        $this->app_mock = $this->createApplication();
+    }
+
+    /**
+     * Build and seed the database to an sqlite database on memory
+     */
+    public function loadDatabase(): void
+    {
+        $capsule = new Capsule;
+        $capsule->addConnection([
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+        $capsule->bootEloquent();
+        $capsule->setAsGlobal();
+        $this->db = $capsule->getDatabaseManager();
+        $capsule->schema()->create('users', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function ObjectConstructionOkWhenDB_LOGGER_ACTIVEIsOff()
+    {
+        putenv('DB_LOGGER_ACTIVE=false');
+        $this->assertInstanceOf(
+            DatabaseServiceProvider::class,
+            new DatabaseServiceProvider(['auth' => $this->app_mock])
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function ObjectConstructionOkWhenDB_LOGGER_ACTIVEIsOn()
+    {
+        putenv('DB_LOGGER_ACTIVE=true');
+        $this->assertInstanceOf(
+            DatabaseServiceProvider::class,
+            new DatabaseServiceProvider(['auth' => $this->app_mock])
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function DoesNotCallWriteToFileOk()
+    {
+        putenv('DB_LOGGER_ACTIVE=false');
+        $this->loadDatabase();
+        File::shouldReceive('append')->never();
+        for ($i = 0; $i < 10; $i++) {
+            $user = app(User::class);
+            $user->fill([
+                'name' => Str::random(10),
+                'email' => Str::random(10),
+            ]);
+            $user->save();
+        }
+        $user = app(User::class)->where('id', '1')->get();
+        $this->assertInstanceOf(Collection::class, $user);
+        $this->assertInstanceOf(User::class, $user[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function CallWriteToFileOk()
+    {
+        putenv('DB_LOGGER_ACTIVE=true');
+        $this->loadDatabase();
+        File::shouldReceive('append')->andReturn([]);
+        for ($i = 0; $i < 10; $i++) {
+            $user = app(User::class);
+            $user->fill([
+                'name' => Str::random(10),
+                'email' => Str::random(10),
+            ]);
+            $user->save();
+        }
+        $user = app(User::class)->where('id', '1')->get();
+        $this->assertInstanceOf(Collection::class, $user);
+        $this->assertInstanceOf(User::class, $user[0]);
+    }
+}
---
+++ tests/Unit/Laravel/Models/User.php
@@ -0,0 +1,29 @@
+<?php
+
+// namespace Tests\Unit\Laravel\Models;
+
+namespace RStriquer\EloquentLogger\Tests\Unit\Laravel\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// use RStriquer\EloquentLogger\Tests\Unit\Laravel\Factories\UserFactory;
+
+class User extends Model
+{
+    // use HasFactory;
+
+    public $timestamps = false;
+
+    protected $guarded = [];
+
+    // /**
+    //  * Create a new factory instance for the model.
+    //  *
+    //  * @return \Illuminate\Database\Eloquent\Factories\Factory<static>
+    //  */
+    // protected static function newFactory()
+    // {
+    //     return new UserFactory();
+    // }
+}
