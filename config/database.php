ab9c8b28d9b6f152d402a6e5135ddbff2718e34c
rstriquer <rstriquer@gmail.com>
2023-04-19 13:02:48 -0300

    feat: Add logging feature to the project

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'rstriquer/eloquent-logger' => [
+        /**
+         * Turns off logging while the value is false.
+         */
+        'active' => env('DB_LOGGER_ACTIVE', false),
+        /**
+         * The log file name to use from within storage/logs directory.
+         */
+        'file' => env('DB_LOGGER_FILE', '/logs/query.log'),
+    ],
+];
---
+++ src/Adapters/Laravel/DatabaseServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RStriquer\EloquentLogger\Adapters\Laravel;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Service provider that attach the Eloquent Console Manager listener.
+ */
+class DatabaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (config('database.rstriquer/eloquent-logger.active')) {
+            DB::listen(function ($query) {
+                $buf = str_replace(
+                    ['?'], ['%s'], str_replace('%', '%%', $query->sql)
+                );
+                if (is_array($buf)) {
+                    $buf = implode('', $buf);
+                }
+                File::append(
+                    storage_path(config('database.rstriquer/eloquent-logger.file')),
+                    '['.date('Y-m-d H:i:s').' query time:'.$query->time
+                        .']'.PHP_EOL.vsprintf($buf, $query->bindings)
+                        .PHP_EOL.PHP_EOL
+                );
+            });
+        }
+    }
+}
