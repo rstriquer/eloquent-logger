4c696167e122f4de2b46024906da991cd7f03427
rstriquer <rstriquer@gmail.com>
2023-04-19 23:06:33 -0300

    docs: Delete unnecessary comments

--- tests/Unit/Laravel/Models/User.php
+++ tests/Unit/Laravel/Models/User.php
@@ -1,29 +1,11 @@
 <?php
 
-// namespace Tests\Unit\Laravel\Models;
-
 namespace Rstriquer\EloquentLogger\Tests\Unit\Laravel\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-// use RStriquer\EloquentLogger\Tests\Unit\Laravel\Factories\UserFactory;
 
 class User extends Model
 {
-    // use HasFactory;
-
     public $timestamps = false;
-
     protected $guarded = [];
-
-    // /**
-    //  * Create a new factory instance for the model.
-    //  *
-    //  * @return \Illuminate\Database\Eloquent\Factories\Factory<static>
-    //  */
-    // protected static function newFactory()
-    // {
-    //     return new UserFactory();
-    // }
 }
